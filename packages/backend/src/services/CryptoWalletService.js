a606b24f640c91713a155747895ab3fd414355af
D4n13l-D33 <abrahamdanielu793@gmail.com>
2025-10-24 13:24:01 +0100

    Fix Litecoin address generation with proper network parameters
    - Add proper LTC network configuration with correct pubKeyHash (0x30)
    - LTC addresses will now start with 'L' instead of Bitcoin's '1'
    - Fixes BlockCypher API error: 'Address incompatible with LTC.main'
    - Add LTC testnet configuration as well

--- packages/backend/src/services/CryptoWalletService.js
+++ packages/backend/src/services/CryptoWalletService.js
@@ -48,9 +48,29 @@ class CryptoWalletService {
     // Network configurations
     this.networks = {
       BTC: bitcoin.networks.bitcoin,
-      LTC: bitcoin.networks.litecoin,
+      LTC: {
+        messagePrefix: '\x19Litecoin Signed Message:\n',
+        bech32: 'ltc',
+        bip32: {
+          public: 0x019da462,
+          private: 0x019d9cfe
+        },
+        pubKeyHash: 0x30,
+        scriptHash: 0x32,
+        wif: 0xb0
+      },
       BTC_TESTNET: bitcoin.networks.testnet,
-      LTC_TESTNET: bitcoin.networks.testnet
+      LTC_TESTNET: {
+        messagePrefix: '\x19Litecoin Signed Message:\n',
+        bech32: 'tltc',
+        bip32: {
+          public: 0x0436f6e1,
+          private: 0x0436ef7d
+        },
+        pubKeyHash: 0x6f,
+        scriptHash: 0x3a,
+        wif: 0xef
+      }
     };
   }
 
